67eb74466661c877367cd448e8a97d00e7743c06
dannykrug <dannykrug@gmail.com>
2019-05-04 18:42:16 -0400

    game complete

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const userScore = 0;
-const cpuScore = 0;
+let userScore = 0;
+let cpuScore = 0;
 const userScore_span = document.getElementById('user-score');
 const cpuScore_span = document.getElementById('cpu-score');
 const scoreBoard_div = document.querySelector('.score-board');
-const result_div = document.querySelector('.result');
+const result_div = document.querySelector('.result > p');
 const rock_div = document.getElementById('rock');
 const paper_div = document.getElementById('paper');
 const scissors_div = document.getElementById('scissors');
@@ -11,42 +11,60 @@ const scissors_div = document.getElementById('scissors');
 main();
 
 function getCpuChoice() {
-  const choices = ["rock", "paper", "scissors"]
+  const choices = ["Rock", "Paper", "Scissors"]
   const randomNumber = Math.floor(Math.random() * 3);
   return choices[randomNumber];
+}
+
+function win(user, cpu) {
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  cpuScore_span.innerHTML = cpuScore;
+  result_div.innerHTML = `${user} beats ${cpu}. You Win!`
+}
+
+function lose(user, cpu) {
+  cpuScore++
+  userScore_span.innerHTML = userScore;
+  cpuScore_span.innerHTML = cpuScore;
+  result_div.innerHTML = `${cpu} beats ${user}. You Lose!`
+}
+
+function tie(user, cpu) {
+  result_div.innerHTML = `${user} ties ${cpu}. It's a Draw!`
 }
 
 function game(userChoice) {
   const cpuChoice = getCpuChoice();
   switch (userChoice + cpuChoice) {
-    case "rockscissors":
-    case "paperrock":
-    case "scissorspaper":
-      console.log("user wins!");
+    case "RockScissors":
+    case "PaperRock":
+    case "ScissorsPaper":
+      win(userChoice, cpuChoice);
       break;
-    case "rockpaper":
-    case "paperscissors":
-    case "scissorsrock":
-      console.log("user loses!");
+    case "RockPaper":
+    case "PaperScissors":
+    case "ScissorsRock":
+      lose(userChoice, cpuChoice);
       break;
-    case "rockrock":
-    case "paperpaper":
-    case "scissorsscissors":
-      console.log("it's a tie!");
+    case "RockRock":
+    case "PaperPaper":
+    case "ScissorsScissors":
+      tie(userChoice, cpuChoice);
       break;
   }
 }
 
 function main() {
   rock_div.addEventListener('click', function() {
-    game("rock")
+    game("Rock")
   })
 
   paper_div.addEventListener('click', function() {
-    game("paper")
+    game("Paper")
   })
 
   scissors_div.addEventListener('click', function() {
-    game("scissors")
+    game("Scissors")
   })
 }
